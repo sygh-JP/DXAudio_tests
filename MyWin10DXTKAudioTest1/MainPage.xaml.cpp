53137ca885829a26a4b482ab78f4ed93d69282ea
sygh-JP <whitekatyusha@yahoo.co.jp>
2018-02-26 22:59:53 +0900

    Appended sample for Windows 10 UWP.

---
+++ MyWin10DXTKAudioTest1/MainPage.xaml.cpp
@@ -0,0 +1,178 @@
+﻿//
+// MainPage.xaml.cpp
+// MainPage クラスの実装。
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace MyWin10DXTKAudioTest1;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Popups;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+	MyAudioHelpers::MyAudioManager::Startup();
+
+	m_dispatcherTimer = ref new DispatcherTimer();
+	TimeSpan span;
+	span.Duration = 1000 * 1000 * 10 / FramesPerSecond; // 単位は100[ns]。
+	m_dispatcherTimer->Interval = span;
+	m_dispatcherTimer->Tick += ref new Windows::Foundation::EventHandler<Platform::Object ^>(this, &MainPage::OnTick);
+
+	const LPCWSTR seAudioFileNames[MyAudioSet1::SoundEffectsSlotCount] =
+	{
+		L"se_maoudamashii_magical01.wav",
+		L"se_maoudamashii_magical02.wav",
+		L"se_maoudamashii_magical03.wav",
+		L"se_maoudamashii_magical07.wav",
+	};
+
+	m_audioSet = std::make_unique<MyAudioSet1>();
+
+	String^ outerErrMsg = nullptr;
+	try
+	{
+		m_audioSet->CreateAudioEngineSE();
+
+		for (int index = 0; index < MyAudioSet1::SoundEffectsSlotCount; ++index)
+		{
+			const LPCWSTR seFilePath = seAudioFileNames[index];
+			m_audioSet->m_soundEffects[index] = std::make_unique<DirectX::SoundEffect>(m_audioSet->m_audioEngineSE.get(), seFilePath);
+			m_audioSet->m_soundEffectInstances[index] = m_audioSet->m_soundEffects[index]->CreateInstance();
+		}
+
+		m_audioSet->m_audioManager = std::make_unique<MyAudioHelpers::MyAudioManager>();
+		m_audioSet->m_audioManager->Init();
+		m_audioSet->m_audioManager->SetMasterVolume(0.1f);
+
+		String^ bgmAudioFileName = L"bgm_maoudamashii_healing01.mp3";
+
+		auto loadBgmFileTask = MyUtils::CreateMemoryStreamFromLocalFileAsync(bgmAudioFileName);
+		loadBgmFileTask.then([this](Windows::Storage::Streams::InMemoryRandomAccessStream^ memStream)
+		{
+			m_audioSet->m_audioPlayerBGM = m_audioSet->m_audioManager->CreateAudioPlayerUniquePtr(
+				MyAudioHelpers::MyAudioManager::CreateSourceReader(
+				MyAudioHelpers::MyAudioManager::CreateByteStream(memStream)));
+		}).then([](concurrency::task<void> endTask)
+		{
+			String^ innerErrMsg = nullptr;
+			try
+			{
+				endTask.get();
+			}
+			catch (const MyComHelpers::MyComException& ex)
+			{
+				innerErrMsg = ref new String(ex.GetErrorMessage());
+			}
+			catch (const std::exception& ex)
+			{
+				innerErrMsg = ref new String(CStringW(ex.what()));
+			}
+			catch (Platform::Exception^ ex)
+			{
+				innerErrMsg = ex->Message;
+			}
+			if (innerErrMsg != nullptr)
+			{
+				auto dialog = ref new MessageDialog(innerErrMsg, L"Error");
+				concurrency::create_task(dialog->ShowAsync()).then([](IUICommand^) {});
+			}
+		});
+	}
+	catch (const MyComHelpers::MyComException& ex)
+	{
+		outerErrMsg = ref new String(ex.GetErrorMessage());
+	}
+	catch (const std::exception& ex)
+	{
+		outerErrMsg = ref new String(CStringW(ex.what()));
+	}
+	catch (Platform::Exception^ ex)
+	{
+		outerErrMsg = ex->Message;
+	}
+	if (outerErrMsg != nullptr)
+	{
+		auto dialog = ref new MessageDialog(outerErrMsg, L"Error");
+		concurrency::create_task(dialog->ShowAsync()).then([](IUICommand^) {});
+	}
+}
+
+
+void MyWin10DXTKAudioTest1::MainPage::buttonPlay_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	if (m_audioSet->m_audioPlayerBGM)
+	{
+		m_audioSet->m_audioPlayerBGM->Play();
+	}
+	m_dispatcherTimer->Start();
+}
+
+
+void MyWin10DXTKAudioTest1::MainPage::buttonPause_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	if (m_audioSet->m_audioPlayerBGM)
+	{
+		m_audioSet->m_audioPlayerBGM->Pause();
+	}
+}
+
+
+void MyWin10DXTKAudioTest1::MainPage::buttonStop_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	m_dispatcherTimer->Stop();
+	if (m_audioSet->m_audioPlayerBGM)
+	{
+		m_audioSet->m_audioPlayerBGM->Stop();
+	}
+}
+
+
+void MyWin10DXTKAudioTest1::MainPage::buttonSE0_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	m_audioSet->PlaySE(0);
+}
+
+
+void MyWin10DXTKAudioTest1::MainPage::buttonSE1_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	m_audioSet->PlaySE(1);
+}
+
+
+void MyWin10DXTKAudioTest1::MainPage::buttonSE2_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	m_audioSet->PlaySE(2);
+}
+
+
+void MyWin10DXTKAudioTest1::MainPage::buttonSE3_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	m_audioSet->PlaySE(3);
+}
+
+
+void MyWin10DXTKAudioTest1::MainPage::OnTick(Platform::Object ^sender, Platform::Object ^args)
+{
+	//throw ref new Platform::NotImplementedException();
+	if (m_audioSet)
+	{
+		m_audioSet->UpdateSE();
+		m_audioSet->UpdateBGM(this->checkLoop->IsChecked->Value == true);
+	}
+}
---
+++ MyWin10DXTKAudioTest1/pch.h
@@ -0,0 +1,49 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <collection.h>
+#include <ppltasks.h>
+
+#include <vector>
+#include <array>
+
+#include <wrl/client.h>
+
+#include <atlbase.h>
+#include <atlstr.h>
+
+// DirectXTK
+#include <Audio.h>
+
+// Media Foundation
+#include <mfapi.h>
+#include <mfidl.h>
+#include <mfreadwrite.h>
+#pragma comment(lib, "mfuuid.lib")
+#pragma comment(lib, "mfplat.lib")
+#pragma comment(lib, "mfreadwrite.lib")
+
+
+#ifdef _DEBUG
+#if defined(_M_IX86)
+#pragma comment(lib, "Windows10\\Win32\\Debug\\DirectXTK.lib")
+#elif defined(_M_X64)
+#pragma comment(lib, "Windows10\\x64\\Debug\\DirectXTK.lib")
+#elif defined(_M_ARM)
+#pragma comment(lib, "Windows10\\ARM\\Debug\\DirectXTK.lib")
+#endif
+#else
+#if defined(_M_IX86)
+#pragma comment(lib, "Windows10\\Win32\\Release\\DirectXTK.lib")
+#elif defined(_M_X64)
+#pragma comment(lib, "Windows10\\x64\\Release\\DirectXTK.lib")
+#elif defined(_M_ARM)
+#pragma comment(lib, "Windows10\\ARM\\Release\\DirectXTK.lib")
+#endif
+#endif
+
+#include "App.xaml.h"
